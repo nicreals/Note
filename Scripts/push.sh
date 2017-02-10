07760b623915069854ac172c2a60e58b9a69ee47
聂锐 <nic.reals@outlook.com>
2017-02-10 22:39:52 +0800

    update

--- Scripts/push.sh
+++ Scripts/push.sh
@@ -4,12 +4,22 @@
 # 推送markdown文件到master分支
 #
 
+echo "====== making summary ======"
+
 greed-summary -t 'Note' -i ['IMG'，'_book','Scripts'] -S gitbook -a
 
 cp SUMMARY.md README.md
+
+echo ""
+
+echo "====== pushing commit ======"
+
+echo ""
 
 git add ./
 
 git commit -a -m "update"
 
 git push origin master
+
+echo "====== done ======"
