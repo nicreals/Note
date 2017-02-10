a933af54514d296996f5e7565341b24905de350c
nic_reals <nic.reals@outlook.com>
2017-02-10 15:34:48 +0000

    Deploy to GitHub Pages by travis

--- Scripts/push.sh
+++ Scripts/push.sh
@@ -4,15 +4,9 @@
 # 推送markdown文件到master分支
 #
 
-echo "====== making summary ======"
-
 greed-summary -t 'Note' -i ['IMG'，'_book','Scripts'] -S gitbook -a
 
 cp SUMMARY.md README.md
-
-echo ""
-
-echo "====== pushing commit ======"
 
 echo ""
 
@@ -21,5 +15,3 @@ git add ./
 git commit -a -m "update"
 
 git push origin master
-
-echo "====== done ======"
